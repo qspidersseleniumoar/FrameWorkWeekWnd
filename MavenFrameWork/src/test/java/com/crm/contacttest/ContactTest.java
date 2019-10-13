c006d2f18ea9875d8edb454da906592471bb9131
QSP <QSP@testing-env>
2019-10-13 14:22:45 +0530

    my first Commit

---
+++ MavenFrameWork/src/test/java/com/crm/contacttest/ContactTest.java
@@ -0,0 +1,27 @@
+package com.crm.contacttest;
+
+import org.testng.annotations.Test;
+
+public class ContactTest {
+	
+	@Test
+	public void createContact() {
+		System.out.println("execute createContact");
+	}
+	
+	@Test
+	public void searchContact() {
+		System.out.println("execute searchContact");
+	}
+	
+	@Test
+	public void deleteContact() {
+		System.out.println("execute dleteContact");
+	}
+	
+	@Test
+	public void editContact() {
+		System.out.println("execute editContact");
+	}
+
+}
---
+++ MavenFrameWork/src/test/java/com/crm/contacttest/ImportConatctTest.java
@@ -0,0 +1,12 @@
+package com.crm.contacttest;
+
+import org.testng.annotations.Test;
+
+public class ImportConatctTest {
+	
+	@Test
+	public void importContact() {
+		System.out.println("execute importContact");
+	}
+
+}
